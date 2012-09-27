ac1643551eaba3ba344c9a5d28547e34ea3db413
InfoPower <nandini.srkr@gmail.com>
2012-09-26 21:02:15 -0400

    change dependency on id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :mainmenu
-  def mainmenu
-    @pagename = Page.find(1).name
-    @nextpages = Page.find_by_parent_id(1)
-  end
 end

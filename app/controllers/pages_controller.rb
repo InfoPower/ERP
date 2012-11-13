be2b23a55a2c1a87b04462b9f9efd415f63b9bc2
InfoPower <nandini.srkr@gmail.com>
2012-11-13 16:25:41 -0500

    Modify Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def index
-    @pages = Page.paginate(:page => params[:page], :per_page => 12)
+    @pages = Page.order(:id).page(params[:page]).per(12)
    
     respond_to do |format|
       format.html # index.html.erb
